e4189d256d321c733d8cbc9e8ca852c1b0e63f32
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-11-07 15:18:14 -0800

    centered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Wizard from "./components/Wizard";
+import { Grid } from "@mui/material";
 
 const App = () => {
   return (
-    <div>
-      <Wizard />
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={3}>
+        <Wizard />
+      </Grid>
+    </Grid>
   );
 };
 
